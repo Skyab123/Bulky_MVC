8faaac72447096c9f7cfd4e3f7b560967a098546
Mattis Callens <mattis.callens.2004@gmail.com>
2026-02-05 17:32:33 +0100

    Testing ongedaan gemaakt

--- BulkyWeb/Controllers/HomeController.cs
+++ BulkyWeb/Controllers/HomeController.cs
@@ -16,11 +16,6 @@ namespace BulkyWeb.Controllers
             return View();
         }
 
-        public IActionResult Contact()
-        {
-            return View(); 
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
